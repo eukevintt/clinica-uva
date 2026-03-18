81ed4b2845a3bb232bf551c3be8e4cc31284b7c8
Kevin <kevin.araujo@saveincloud.com>
2026-03-17 23:55:24 -0300

    feat: integra busca de endereco automatica com viacep

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -1,11 +1,18 @@
+const axios = require("axios");
 const Appointment = require("../models/Appointment");
 
 async function createAppointment(req, res) {
     try {
-        const { date, time, cep, street, neighborhood, city, state, weatherAlert } = req.body || {};
+        const { date, time, cep, weatherAlert } = req.body || {};
 
         if (!date || !time || !cep) {
             return res.status(400).json({ message: "Data, horário e CEP são obrigatórios" });
+        }
+
+        const normalizedCep = cep.replace(/\D/g, "");
+
+        if (normalizedCep.length !== 8) {
+            return res.status(400).json({ message: "CEP inválido" });
         }
 
         const appointmentExists = await Appointment.findOne({ date, time });
@@ -14,15 +21,21 @@ async function createAppointment(req, res) {
             return res.status(409).json({ message: "Já existe uma consulta agendada para esse horário" });
         }
 
+        const viaCepResponse = await axios.get(`https://viacep.com.br/ws/${normalizedCep}/json/`);
+
+        if (viaCepResponse.data.erro) {
+            return res.status(400).json({ message: "CEP não encontrado" });
+        }
+
         const appointment = await Appointment.create({
             patient: req.user.id,
             date,
             time,
-            cep,
-            street: street || "",
-            neighborhood: neighborhood || "",
-            city: city || "",
-            state: state || "",
+            cep: normalizedCep,
+            street: viaCepResponse.data.logradouro || "",
+            neighborhood: viaCepResponse.data.bairro || "",
+            city: viaCepResponse.data.localidade || "",
+            state: viaCepResponse.data.uf || "",
             weatherAlert: weatherAlert || ""
         });
 
