69496cb7e846bb1a83c37a836acda28533ff621e
Kevin <kevin.araujo@saveincloud.com>
2026-03-18 21:04:00 -0300

    alteracoes 2

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'clinica_db'
+        })
         console.log("MongoDB conectado com sucesso");
     } catch (error) {
         console.error("Erro ao conectar no MongoDB:", error.message);
